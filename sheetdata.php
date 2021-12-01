5a043376d8695e942d4ef7b292405500a7cfb1d3
Acquilla <90166904+acquillac@users.noreply.github.com>
2021-11-30 21:16:40 -0700

    commenting

--- sheetdata.php
+++ sheetdata.php
@@ -8,11 +8,11 @@ $client = new \Google_Client();
   $client->setAccessType('offline');
   $client->setAuthConfig(__DIR__ . './credentials.json');
   $service = new Google_Service_Sheets($client);
-  $spreadsheetID = "1EDH-0WPCppDsfTYCAiEcf7mSmfyDf8AnlBXgNizQ4M8";
+ 
+ //https://docs.google.com/spreadsheets/d/  ID should be here /edit#gid=0   
+  $spreadsheetID = "Google Speadsheet ID Here";
 
   $sheetData = [];
-
-
   $range = "Sheet1!A:B";
   $response = $service->spreadsheets_values->get($spreadsheetID, $range);
   $values = $response->getValues();
@@ -20,13 +20,13 @@ $client = new \Google_Client();
     print "No data found \n";
   } else {
     
-    
+  // adding sheet data to an empty array     
     foreach ($values as $row) {
       array_push($sheetData, [$row[0], $row[1]]);
     }
     
+  // writing sheet data to json file     
     $jsonData = json_encode(array('data'=>$sheetData));
-
     file_put_contents("data.json", $jsonData);
   }
 
